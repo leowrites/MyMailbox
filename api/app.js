19be6b5f8df41806308a7daa07ad7d8ee283fdef
leowrites <75044178+leowrites@users.noreply.github.com>
2022-05-27 23:29:28 -0700

    added route for requesting auth status

--- api/app.js
+++ api/app.js
@@ -1,24 +1,47 @@
 const express = require('express');
-
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const app = express();
+const ejsMate = require('ejs-mate')
 const indexRouter = require('./routes/mail/index')
+const ExpressError = require('./error')
+const path = require('path');
+const session = require('cookie-session')
+const cors = require('cors')
+
+
+const sessionConfig = {
+  name: 'session',
+  secret: '6D7249AF5C762F9D47E91A2375F71',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+  }
+}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// app.use(hpp())
+app.use(cors())
+app.set('trust proxy', 1)
+app.use(session(sessionConfig))
 
+app.set('views', path.resolve() + '/views')
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.use('/api/mail', indexRouter);
-
+app.use('/api/mail', indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(new ExpressError(404));
 });
 
 // error handler
@@ -29,6 +52,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  console.log(err)
   res.render('error');
 });
 
--- api/routes/mail/index.js
+++ api/routes/mail/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const path = require('path');
 const { google } = require('googleapis')
 require('dotenv').config({
-    path: path.join(__dirname, '/.env')
-  })
-// const cors = require('cors')
-// router.use(cors)
+    path: path.join(path.resolve(), '.env')
+})
 
 const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
@@ -24,19 +22,38 @@ const gmail = google.gmail({
     auth: oauth2Client
 })
 
-
 router.get('/', async (req, res, next) => {
-    let isAuthenticated = false
-    if (req.query.code){
+    console.log(req.query.code)
+    if (req.query.code) {
         const { tokens } = await oauth2Client.getToken(req.query.code)
         oauth2Client.setCredentials(tokens)
-        isAuthenticated = true
+
+        // need to tell client the user is authenticated
+        const messages = await gmail.users.messages.list({
+            userId: 'me'
+        })
+        const userData = await gmail.users.getProfile({
+            userId: 'me'
+        })
+        // session isn't saving data upon refreshing
+        // req.session.userProfile = userData.data
+        // req.session.userMessages = messages.data
+        res.redirect('http://localhost:3000/appage')
     }
-    res.send(isAuthenticated)
 });
 
-router.post('/', (req, res, next) => {
-    // console.log(url)
+// Get user authentication information
+router.get('/auth', async(req, res, next) => {
+    console.log('request received!')
+    const userData = await gmail.users.getProfile({
+        userId: 'me'
+    })
+    res.json(userData.data)
+})
+
+
+// Redirect user to google authentication
+router.post('/login', (req, res, next) => {
     res.redirect(url)
 })
 
