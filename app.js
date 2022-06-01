5100c53750ec0612f6b35d1ced9d373e31c03b28
leowrites <75044178+leowrites@users.noreply.github.com>
2022-05-31 23:48:27 -0700

    fix package

--- app.js
+++ app.js
@@ -59,14 +59,14 @@ app.set('views', path.resolve() + '/views')
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.resolve(__dirname, '../../client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 app.use('/api/mail', indexRouter)
 app.locals.gmail = ''
 
 
 // catch 404 and forward to error handler
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
 })
 app.use(function (req, res, next) {
   next(new ExpressError(404));
